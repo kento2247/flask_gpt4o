dc1874f85c390c5e1ad7579d4597720f32813a56
KentoTokura <tkento1985@icloud.com>
2024-08-28 15:14:42 +0900

    change json

---
+++ src/line_a.py
@@ -0,0 +1,107 @@
+import json
+
+import requests
+import yaml
+
+
+class line:
+    def __init__(self, channel_access_token: str):
+        self.channnel_access_token = channel_access_token
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.channnel_access_token}",
+        }
+        config = yaml.safe_load(open("config.yaml"))
+        self.config = config["line"]
+
+    def reply(self, reply_token: str, messages: list):
+        response_json = {
+            "replyToken": reply_token,
+            "messages": messages,
+        }
+        response = requests.post(
+            "https://api.line.me/v2/bot/message/reply",
+            headers=self.headers,
+            json=response_json,
+        )
+        if response.status_code != 200:
+            raise Exception(response.text)
+
+    def get_profile(self, user_id: str) -> dict:
+        response = requests.get(
+            f"https://api.line.me/v2/bot/profile/{user_id}",
+            headers=self.headers,
+        )
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(response.text)
+
+    def reply_gpt_response(self, reply_token: str, session_id: str, message: str):
+        json_path = self.config["line"]["template_path"]["gpt_response"]
+        template = json.load(open(json_path))
+        template["body"]["contents"][0]["text"] = session_id
+        template["body"]["contents"][1]["contents"][1]["contents"][1]["text"] = message
+
+        response_json = {
+            "replyToken": reply_token,
+            "messages": [
+                {
+                    "type": "flex",
+                    "altText": "This is a Flex Message",
+                    "contents": template,
+                }
+            ],
+        }
+        response = requests.post(
+            "https://api.line.me/v2/bot/message/reply",
+            headers=self.headers,
+            json=response_json,
+        )
+        if response.status_code != 200:
+            raise Exception(response.text)
+
+    def reply_interview_end(self, reply_token: str):
+        json_path = self.config["line"]["template_path"]["interview_end"]
+        template = json.load(open(json_path))
+        response_json = {
+            "replyToken": reply_token,
+            "messages": [
+                {
+                    "type": "flex",
+                    "altText": "This is a Flex Message",
+                    "contents": template,
+                }
+            ],
+        }
+        response = requests.post(
+            "https://api.line.me/v2/bot/message/reply",
+            headers=self.headers,
+            json=response_json,
+        )
+        if response.status_code != 200:
+            raise Exception(response.text)
+
+    def push_gpt_response(self, line_id: str, session_id: str, message: str):
+        json_path = self.config["line"]["template_path"]["gpt_response"]
+        template = json.load(open(json_path))
+        template["body"]["contents"][0]["text"] = session_id
+        template["body"]["contents"][1]["contents"][1]["contents"][1]["text"] = message
+
+        response_json = {
+            "to": line_id,
+            "messages": [
+                {
+                    "type": "flex",
+                    "altText": "This is a Flex Message",
+                    "contents": template,
+                }
+            ],
+        }
+        response = requests.post(
+            "https://api.line.me/v2/bot/message/push",
+            headers=self.headers,
+            json=response_json,
+        )
+        if response.status_code != 200:
+            raise Exception(response.text)
